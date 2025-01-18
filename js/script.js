7eae3d8574559c27001554b0a4422f04e57adfb2
JacekWoloszczak <jacek.woloszczak@gmail.com>
2025-01-18 18:00:01 +0100

    przepchanie zmian

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const importantLi= document.querySelector(".important_li")
 const navigationDiv= document.querySelector(".navigation");
 const SQUARE= 5508;
 const MOSAIC= 200;
-const MOSAICMAIN=750;
+const MOSAICMAIN=740;
 
 function getRandomColor() {
     
