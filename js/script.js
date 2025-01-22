c7954836626896dac80a5b642fb32c5ed40b41e8
JacekWoloszczak <jacek.woloszczak@gmail.com>
2025-01-22 19:20:02 +0100

    poprawiona nawigacja

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const pallette= ["#a7a896", "#636a72", "#090808", "#4f545c"];
 const mosaic= document.querySelector(".mosaic");
 const mosaicMain= document.querySelector(".mosaic_main");
 const mosaicPallette= ["#a89574", "#902000", "#9e6000", "#470c00","#6e4204"];
-const unseenList= document.querySelector(".navigation_second_deep")
+const unseenList= document.querySelector(".navigation_second_deep_container")
 const importantLi= document.querySelector(".important_li")
 const navigationDiv= document.querySelector(".navigation");
 const mosaicDown= document.querySelector(".mosaic_footer");
 // const SQUARE= 5508;
 const MOSAIC= 400;
-const MOSAICMAIN=1200;
-const MOSAICFOOTER= 100;
+const MOSAICMAIN=1400;
+const MOSAICFOOTER= 150;
 
 const weAreHere = document.querySelector(".we_are_here");
 const hideMap = document.querySelector(".container_with_map_wrapper");
@@ -71,8 +71,9 @@ for (let i = 0; i < MOSAICFOOTER; i++){
       
 }
   importantLi.addEventListener("click", ()=>{
-    unseenList.classList.toggle("d_none");
-    unseenList.classList.toggle("d_flex");
+    unseenList.classList.toggle("is-hidden");
+    
+ 
    
  
   });
